83f3fc8b2d2319e23e0398c54660a736ae11ea32
Jorge Orellana <1998jorgeorellana@gmail.com>
2023-08-26 01:42:59 -0600

    Actualización de crear secciones

--- src/Docentes/Helpers/convertirFecha.js
+++ src/Docentes/Helpers/convertirFecha.js
@@ -5,3 +5,17 @@ export const convertirFechaAño = (fechaISO) => {
     
     return `${anio}`;
   }
+
+  export const HorasInicioFin = () => {
+    const horas = ['06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21'];
+      
+    const minutos = [];
+    for (let minute = 0; minute < 60; minute++) {
+      minutos.push(`${minute.toString().padStart(2, "0")}`);
+    }
+  
+    return {
+      horas: horas,
+      minutos: minutos,
+    };
+  }
