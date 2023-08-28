b7fb5972eb030920d9df1016cb0fefc532b34d88
Fernando Garcia <gfernandoentique37@gmail.com>
2023-08-27 20:49:07 -0600

    cambios 8:49

--- src/Administardor/pages/lidtsE.jsx
+++ src/Administardor/pages/lidtsE.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export const EstudiantesComponent = () => {
   const [estudiantes, setEstudiantes] = useState([]);
-  const [filtroNombre, setFiltroNombre] = useState('');
-  const [filtroNumCuenta, setFiltroNumCuenta] = useState('');
-  const [filtroCarrera, setFiltroCarrera] = useState('');
+  const [filtroNombre, setFiltroNombre] = useState("");
+  const [filtroNumCuenta, setFiltroNumCuenta] = useState("");
+  const [filtroCarrera, setFiltroCarrera] = useState("");
   const [paginaActual, setPaginaActual] = useState(1);
   const elementosPorPagina = 10; // Cantidad de registros por página
 
@@ -15,7 +15,7 @@ export const EstudiantesComponent = () => {
         const data = await response.json();
         setEstudiantes(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -23,74 +23,142 @@ export const EstudiantesComponent = () => {
   }, [paginaActual]);
 
   // Lógica de filtrado y paginación
-  const estudiantesFiltrados = estudiantes.filter(estudiante =>
-    estudiante.primer_nombre.toLowerCase().includes(filtroNombre.toLowerCase()) &&
-    estudiante.num_cuenta.toString().includes(filtroNumCuenta) &&
-    estudiante.nombre_carrera.toLowerCase().includes(filtroCarrera.toLowerCase())
+  const estudiantesFiltrados = estudiantes.filter(
+    (estudiante) =>
+      estudiante.primer_nombre
+        .toLowerCase()
+        .includes(filtroNombre.toLowerCase()) &&
+      estudiante.num_cuenta.toString().includes(filtroNumCuenta) &&
+      estudiante.nombre_carrera
+        .toLowerCase()
+        .includes(filtroCarrera.toLowerCase())
   );
 
-  const totalPaginas = Math.ceil(estudiantesFiltrados.length / elementosPorPagina);
+  const totalPaginas = Math.ceil(
+    estudiantesFiltrados.length / elementosPorPagina
+  );
   const indiceUltimoElemento = paginaActual * elementosPorPagina;
   const indicePrimerElemento = indiceUltimoElemento - elementosPorPagina;
-  const estudiantesPaginados = estudiantesFiltrados.slice(indicePrimerElemento, indiceUltimoElemento);
+  const estudiantesPaginados = estudiantesFiltrados.slice(
+    indicePrimerElemento,
+    indiceUltimoElemento
+  );
 
-  const cambiarPagina = nuevaPagina => {
+  const cambiarPagina = (nuevaPagina) => {
     if (nuevaPagina >= 1 && nuevaPagina <= totalPaginas) {
       setPaginaActual(nuevaPagina);
     }
   };
 
+  const regresar = () => {
+    window.history.back();
+  };
+
   return (
-    <div>
-      <h1>Lista de Estudiantes</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Filtrar por nombre"
-          value={filtroNombre}
-          onChange={e => setFiltroNombre(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Filtrar por número de cuenta"
-          value={filtroNumCuenta}
-          onChange={e => setFiltroNumCuenta(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Filtrar por carrera"
-          value={filtroCarrera}
-          onChange={e => setFiltroCarrera(e.target.value)}
-        />
+    <>
+      <div className="container">
+        <div className="my-4">
+          <button className="btn btn-primary" onClick={regresar}>
+            Atrás
+          </button>
+        </div>
+        <div className="row">
+          <div className="col my-3">
+            <h2 className="text-center">Lista de Estudiantes</h2>
+          </div>
+        </div>
+        <div className="row my-4">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Filtrar por nombre"
+              value={filtroNombre}
+              onChange={(e) => setFiltroNombre(e.target.value)}
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Filtrar por número de cuenta"
+              value={filtroNumCuenta}
+              onChange={(e) => setFiltroNumCuenta(e.target.value)}
+            />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Filtrar por carrera"
+              value={filtroCarrera}
+              onChange={(e) => setFiltroCarrera(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="col">
+          <table className="table table-striped my-3">
+            <thead>
+              <tr>
+                <th scope="col" className="text-center">
+                  Número de Cuenta
+                </th>
+                <th scope="col" className="text-center">
+                  Nombre
+                </th>
+                <th scope="col" className="text-center">
+                  Apellidos
+                </th>
+                <th scope="col" className="text-center">
+                  Correo Institucional
+                </th>
+                <th scope="col" className="text-center">
+                  Carrera
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {estudiantesPaginados.map((estudiante) => (
+                <tr key={estudiante.num_cuenta}>
+                  <td scope="row" className="text-center">
+                    {estudiante.num_cuenta}
+                  </td>
+                  <td scope="row" className="text-center">
+                    {estudiante.primer_nombre} {estudiante.segundo_nombre}
+                  </td>
+                  <td scope="row" className="text-center">
+                    {estudiante.primer_apellido} {estudiante.segundo_apellido}
+                  </td>
+                  <td scope="row" className="text-center">
+                    {estudiante.correo_institucional}
+                  </td>
+                  <td scope="row" className="text-center">
+                    {estudiante.nombre_carrera}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="d-flex justify-content-center">
+            <div className="btn-group w-25 ">
+              <button
+                className="btn btn-primary"
+                onClick={() => cambiarPagina(paginaActual - 1)}
+                disabled={paginaActual === 1}
+              >
+                Anterior
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => cambiarPagina(paginaActual + 1)}
+                disabled={paginaActual === totalPaginas}
+              >
+                Siguiente
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Número de Cuenta</th>
-            <th>Nombre</th>
-            <th>Apellidos</th>
-            <th>Correo Institucional</th>
-            <th>Carrera</th>
-          </tr>
-        </thead>
-        <tbody>
-          {estudiantesPaginados.map(estudiante => (
-            <tr key={estudiante.num_cuenta}>
-              <td>{estudiante.num_cuenta}</td>
-              <td>{estudiante.primer_nombre} {estudiante.segundo_nombre}</td>
-              <td>{estudiante.primer_apellido} {estudiante.segundo_apellido}</td>
-              <td>{estudiante.correo_institucional}</td>
-              <td>{estudiante.nombre_carrera}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>Anterior</button>
-        <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={paginaActual === totalPaginas}>Siguiente</button>
-      </div>
-    </div>
+    </>
   );
 };
-
- 
--- src/Docentes/components/Estadisticas.jsx
+++ src/Docentes/components/Estadisticas.jsx
@@ -14,61 +14,71 @@ export const Estadisticas = () => {
   return (
     <>
       <div className="container">
-        <button className="btn btn-success my-4" onClick={regresar}>
-          Atrás
-        </button>
+        <div className="row  my-4">
+          <div className="col">
+            <button className="btn btn-success" onClick={regresar}>
+              Atrás
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <PowerBIEmbed
+              className="w-100"
+              embedConfig={{
+                type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+                id: "f1cea6c4-1397-4e0d-bb70-77e3ce148b58",
+                embedUrl:
+                  "https://app.powerbi.com/reportEmbed?reportId=f1cea6c4-1397-4e0d-bb70-77e3ce148b58&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUNFTlRSQUwtVVMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19",
+                accessToken:
+                  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMzNhOTM5OTAtOGJiNC00YjlkLWI0MjItNGY4ODUxMjE3ZDdlLyIsImlhdCI6MTY5MzE4NzU0MywibmJmIjoxNjkzMTg3NTQzLCJleHAiOjE2OTMxOTI0MjgsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VUFBQUFMUnluK3NIZFM3bTF0emZFUWZjeGFUcHNFU1RGcDhadjNPMXBDMTAvOHMyd0VYajljL1Yyb2NHSFN6SEZYek5jIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiR0FSQ0lBIENBU1RFTExBTk9TIiwiZ2l2ZW5fbmFtZSI6IkZFUk5BTkRPIEVOUklRVUUiLCJpcGFkZHIiOiIxOTAuNTMuMjQ5LjIwOCIsIm5hbWUiOiJGRVJOQU5ETyBFTlJJUVVFIEdBUkNJQSBDQVNURUxMQU5PUyIsIm9pZCI6IjE1OGEwMTExLTc3MmItNDJkZi05YmRiLTllZmQzYzdlNDcxYSIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS03NzgxMTMyOTUtMjAyMDU4MzM4NC0xMzIwMTc5MC0zNzAyODEiLCJwdWlkIjoiMTAwMzAwMDBBNzA2QzNDRiIsInJoIjoiMC5BVmtBa0RtcE03U0xuVXUwSWstSVVTRjlmZ2tBQUFBQUFBQUF3QUFBQUFBQUFBQlpBSWMuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiSE5FaGt6aUdUT0lJcTVaQmlHRlBHMGRVdkY0YXhScFExM3hydWo1TWQxMCIsInRpZCI6IjMzYTkzOTkwLThiYjQtNGI5ZC1iNDIyLTRmODg1MTIxN2Q3ZSIsInVuaXF1ZV9uYW1lIjoiZmVnYXJjaWFjQHVuYWguaG4iLCJ1cG4iOiJmZWdhcmNpYWNAdW5haC5obiIsInV0aSI6InVwYWtKMGt5TTBxZkhMUzhoWkN0QUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.ViD6y0ABuAD2la8V3O5Ds3gSYqu4olC7nq_vc65Oz2m731kC2USq8wMxgv8fYoyN8wLGb6jk5vFFaipy3Varg6HBGohT1xuVGn-s4tgxl_rEPIJNpiX7ZVPs3NIdBOTZr_8rFRJ-uLlucoWxmNvSCTHe9H_voC6luevlPXAljhcOaGKn-GwaazC0EJgNKSgIxFP87sPqNvb5J6amGZHz2Xjwp9MMNT-JfSzdG0w5DlzFOSDjhognHz22cNCRVyDIaq29ZrzQ3Vvd3kv1yizI_dXcBdAWcSA6GR4m-HvB76t9VfrxlmxeoDzsjAUExnTg7fMLPj7aDLrfCvTJhsiwRA",
+                tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+                settings: {
+                  panes: {
+                    filters: {
+                      expanded: false,
+                      visible: true,
+                    },
+                  },
+                },
+              }}
+              eventHandlers={
+                new Map([
+                  [
+                    "loaded",
+                    function () {
+                      console.log("Report loaded");
+                    },
+                  ],
+                  [
+                    "rendered",
+                    function () {
+                      console.log("Report rendered");
+                    },
+                  ],
+                  [
+                    "error",
+                    function (event) {
+                      console.log(event.detail);
+                    },
+                  ],
+                  ["visualClicked", () => console.log("visual clicked")],
+                  ["pageChanged", (event) => console.log(event)],
+                ])
+              }
+              cssClassName={"Embed-container"}
+              getEmbeddedComponent={(embeddedReport) => {
+                window.report = embeddedReport;
+              }}
+            />
+          </div>
+        </div>
+
         <div className="row">
           <div className="col">
             <div className="App">
               <header className="App-header">
                 {/* <img src={logo} className="App-logo" alt="logo" /> */}
-                <PowerBIEmbed
-                  embedConfig={{
-                    type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-                    id: "81dd1d52-a570-4d59-9aeb-3234ffcf320c",
-                    embedUrl:
-                      "https://app.powerbi.com/reportEmbed?reportId=81dd1d52-a570-4d59-9aeb-3234ffcf320c&groupId=cadf9fc5-53aa-4a44-8233-6533fdc68f51&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVNPVVRILUNFTlRSQUwtVVMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZX19",
-                    accessToken:
-                      "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMzNhOTM5OTAtOGJiNC00YjlkLWI0MjItNGY4ODUxMjE3ZDdlLyIsImlhdCI6MTY5MjQxNDM2NiwibmJmIjoxNjkyNDE0MzY2LCJleHAiOjE2OTI0MTg3ODEsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VUFBQUFNQWJmcHVLVzk5TnpZQzRpK1FQTHpIVFhPb2hPVlMzMW1SRE41TVExSUFQcmh0OEh1TlhEOGFlMVU4NEp5TjJ2IiwiYW1yIjpbInB3ZCIsInJzYSJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiNWU3YTgwMDMtN2NmNS00ZGU3LTg4YmItY2E5YzI2YjBhMmU3IiwiZmFtaWx5X25hbWUiOiJDQVNUUk8gVE9SUkVTIiwiZ2l2ZW5fbmFtZSI6IkNBUkxPUyBKT1NVRSIsImlwYWRkciI6IjE4MS4xMTUuNTkuMjciLCJuYW1lIjoiQ0FSTE9TIEpPU1VFIENBU1RSTyBUT1JSRVMiLCJvaWQiOiJkNGNhMGM2My01ODUwLTQ1ZmMtODQwYi0wZTQ5ZDg2ZmRjN2EiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtNzc4MTEzMjk1LTIwMjA1ODMzODQtMTMyMDE3OTAtMzU0Nzk4IiwicHVpZCI6IjEwMDM3RkZFOUNGOTMwQTEiLCJyaCI6IjAuQVZrQWtEbXBNN1NMblV1MElrLUlVU0Y5ZmdrQUFBQUFBQUFBd0FBQUFBQUFBQUJaQU53LiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6Ii13cVZVUjhTaDZRakpCdHNFVGtzWkNnRnpVWjljUFUwdkQ0LVdCczdPM2ciLCJ0aWQiOiIzM2E5Mzk5MC04YmI0LTRiOWQtYjQyMi00Zjg4NTEyMTdkN2UiLCJ1bmlxdWVfbmFtZSI6ImNjYXN0cm90QHVuYWguaG4iLCJ1cG4iOiJjY2FzdHJvdEB1bmFoLmhuIiwidXRpIjoiR3YydXM5cXROMDI5c2dJdHMzcFZBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.JvsxJI5L2ekmiRbnK4D7aDFMbrXlCZT9x6vG5g91dKSkDkOy2BI37plzEHbI9PCPmwoNKoxKG9vYJ7MJAODVV_KXBK4kAOhPCbOYfz-9HFnaxqmEeew2ZHOX6lBWonsOnV7EZvVMXC4B3UBpGcX1LHvhUh2zXfVeu5o4na8XTG1uV-8OI5B4yWkuC6YrO8_jNtqZclmZhR64KCaN7Y6VWQ8WLBphRYz7dWj2LFfuf5KwmifdpflAFJOrvi9UlgdBG1klmu9gGNqz7BIKxaZ18dApwCVCJYXdYqTV9NBfzFYEtEK7g5BEa1yyd1F3djVkBWpr43ec32qR_3K0TlkzFQ",
-                    tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
-                    settings: {
-                      panes: {
-                        filters: {
-                          expanded: false,
-                          visible: true,
-                        },
-                      },
-                    },
-                  }}
-                  eventHandlers={
-                    new Map([
-                      [
-                        "loaded",
-                        function () {
-                          console.log("Report loaded");
-                        },
-                      ],
-                      [
-                        "rendered",
-                        function () {
-                          console.log("Report rendered");
-                        },
-                      ],
-                      [
-                        "error",
-                        function (event) {
-                          console.log(event.detail);
-                        },
-                      ],
-                      ["visualClicked", () => console.log("visual clicked")],
-                      ["pageChanged", (event) => console.log(event)],
-                    ])
-                  }
-                  cssClassName={"Embed-container"}
-                  getEmbeddedComponent={(embeddedReport) => {
-                    window.report = embeddedReport;
-                  }}
-                />
               </header>
             </div>
           </div>
